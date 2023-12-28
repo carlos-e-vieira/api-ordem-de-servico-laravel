65def02cf765571a6695f1ca749faf45991f8b9a
Carlos Vieira <carlosportalsocial@gmail.com>
2023-12-28 19:45:53 -0300

    added new method to UserServiceTest

--- tests/Unit/Services/UserServiceTest.php
+++ tests/Unit/Services/UserServiceTest.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Services;
 
+use App\Http\Resources\UserResource;
 use App\Services\UserService;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Tests\TestCase;
 
 class UserServiceTest extends TestCase
 {
+    private function getInstance(): UserService
+    {
+        return resolve(UserService::class);
+    }
+
     public function testFileAndPathOfUserService(): void
     {
         $directory = 'app/services';
@@ -21,14 +27,22 @@ class UserServiceTest extends TestCase
 
     public function testGetAllUsers(): void
     {
-        $userService = resolve(UserService::class);
 
         $filters = [];
-        $result = $userService->getAllUsers($filters);
+        $result = $this->getInstance()->getAllUsers($filters);
 
         $this->assertNotEmpty($result);
         $this->assertIsNotArray($result);
         $this->assertIsObject($result);
         $this->assertInstanceOf(LengthAwarePaginator::class, $result);
+    }
+
+    
+    public function testGetUserById(): void
+    {
+        $result = $this->getInstance()->getUserById(1);
+
+        $this->assertInstanceOf(UserResource::class, $result);
+        $this->assertNotEmpty($result);
     }
 }
